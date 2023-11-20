b66b3e92acbc747e79e513fbb422f2608cbd7323
tintin <nithins674@gmail.com>
2023-11-20 23:26:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Dict, Annotated, Optional, Any
+from typing import Dict, Annotated, Optional, Any, Union
 from pydantic import BaseModel, Extra
 from fastapi import FastAPI, Header, Body
 import json
@@ -16,7 +16,7 @@ def read_item(item_id: int, q: Optional[str] = None):
 
 @app.post("/create", response_model=Dict)
 async def create(
-    ticket: Optional[Any] = Body(None),
+    ticket: Optional[Union[Dict, str]] = Body(None),
     x_sero_api_token: str = Header(None),
 ) -> Dict:
     if ticket is not None and isinstance(ticket, str):
